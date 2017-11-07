48cc290410f9e8db983831fd7df6c295cb3fcdb1
Winston Chu <wpchu@andrew.cmu.edu>
2017-11-07 14:30:01 -0500

    initial header stuff

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -2,6 +2,9 @@ var socket = io.connect('/');
 socket.on('players', function (data) {
   console.log(data);
   $("#numPlayers").text(data.number);
+  $("#totalPlayers").text(data.totalPlayers);
+  $("#gamesStarted").text(data.gamesStarted);
+  $("#gamesFinished").text(data.gamesFinished);
 });
 socket.on('welcome', function (data) {
   $("#welcome").text("Welcome player " + data.currPlayer);
--- routes/serverSocket.js
+++ routes/serverSocket.js
@@ -1,19 +1,25 @@
 exports.init = function(io) {
 	var currentPlayers = 0; // keep track of the number of players
 	var playerCounter = 0; // keep track of unique player number
+	var totalPlayers = 0; // keep track of total users ever
+	var gamesStarted = 0; // keep track of number of games started
+	var gamesFinished = 0; // keep track of number of games finished
   // When a new connection is initiated
 	io.sockets.on('connection', function (socket) {
+		++totalPlayers;
 		++currentPlayers;
 		++playerCounter;
 		// Send ("emit") a 'players' event back to the socket that just connected.
-		socket.emit('players', { number: currentPlayers});
+		socket.emit('players', { number: currentPlayers, totalPlayers: totalPlayers, 
+															gamesStarted: gamesStarted, gamesFinished: gamesFinished});
 		socket.emit('welcome', { currPlayer: playerCounter});
 
 		/*
 		 * Emit players events also to all (i.e. broadcast) other connected sockets.
 		 * Broadcast is not emitted back to the current (i.e. "this") connection
      */
-		socket.broadcast.emit('players', { number: currentPlayers});
+		socket.broadcast.emit('players', { number: currentPlayers, totalPlayers: totalPlayers, 
+			gamesStarted: gamesStarted, gamesFinished: gamesFinished});
 		
 		/*
 		 * Upon this connection disconnecting (sending a disconnect event)
